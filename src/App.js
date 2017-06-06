3433aed0a2cb633f4045527c78cf840ccd425de5
xwt向王涛 <wtxiang@ctrip.com>
2017-06-06 18:58:42 +0800

    测试摇动

--- src/App.js
+++ src/App.js
@@ -18,5 +18,19 @@ const styles = StyleSheet.create({
     padding:'5%'
   }
 })
+if(window.DeviceOrientationEvent){
+    var lastAcc;    // 用来存储上一次的deviceorientation事件
+    document.addEventListener('deviceorientation', function(event) {
+        var delA = Math.abs(event.alpha - lastAcc.alpha);    // alpha轴偏转角
+        var delB = Math.abs(event.beta - lastAcc.beta);    // beta轴偏转角
+        var delG = Math.abs(event.gamma - lastAcc.gamma);    // gamma轴偏转角
+        if ( (delA > 15 && delB > 15) || (delA > 15 && delG > 15) || (delB > 15 || delG > 15)) {
+            // 用户设备摇动了，触发响应操作
+            // 此处的判断依据是任意两个轴篇转角度大于15度
+            alert('摇了');
+        }
+        lastAcc = event;    // 存储上一次的event
+    });
+}
 
 export default App
